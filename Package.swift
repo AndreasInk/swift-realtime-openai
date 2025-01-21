42fd2eb630f55ea6f9809440a5d3e6652e21684c
Andreas Ink <andreasink17@gmail.com>
2025-01-20 22:22:53 -0500

    dont use WebRTC on macOS

--- Package.swift
+++ Package.swift
@@ -3,22 +3,28 @@
 import PackageDescription
 
 let package = Package(
-	name: "OpenAI",
-	platforms: [
-		.iOS(.v17),
-		.tvOS(.v17),
-		.macOS(.v14),
-		.watchOS(.v10),
-		.visionOS(.v1),
-		.macCatalyst(.v17),
-	],
-	products: [
-		.library(name: "OpenAI", type: .static, targets: ["OpenAI"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
-	],
-	targets: [
-		.target(name: "OpenAI", dependencies: ["WebRTC"], path: "./src"),
-	]
+    name: "OpenAI",
+    platforms: [
+        .iOS(.v17),
+        .tvOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .visionOS(.v1),
+        .macCatalyst(.v17),
+    ],
+    products: [
+        .library(name: "OpenAI", type: .static, targets: ["OpenAI"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
+    ],
+    targets: [
+        .target(
+            name: "OpenAI",
+            dependencies: [
+                .product(name: "WebRTC", package: "WebRTC", condition: .when(platforms: [.iOS, .tvOS, .visionOS, .macCatalyst]))
+            ],
+            path: "./src"
+        ),
+    ]
 )
